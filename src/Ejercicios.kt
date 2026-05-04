0c0bbcec891128273f5be3bee1ab57d4907bb77f
IvanF-24 <ivanflorez134@gmail.com>
2026-05-03 21:41:17 -0500

    Fun 6

--- src/Ejercicios.kt
+++ src/Ejercicios.kt
@@ -56,3 +56,15 @@ fun reto5() {
     println("Original: ${historial.joinToString()}")
     println("Invertido: ${invertido.joinToString()}")
 }
+// 6. Compatibilidad de Intereses (Match)
+fun reto6() {
+    val interesesP1 = setOf("Tecnología", "Videojuegos", "Fotografía", "Café")
+    val interesesP2 = setOf("Fotografía", "Café", "Fitness", "Emprendimiento")
+
+    val comunes = interesesP1.intersect(interesesP2)
+    val exclusivosP1 = interesesP1.subtract(interesesP2)
+
+    println("\n--- Reto 6: Match de Intereses ---")
+    println("Comunes: $comunes")
+    println("Solo Persona 1: $exclusivosP1")
+}
--- src/Main.kt
+++ src/Main.kt
@@ -7,5 +7,6 @@ fun main() {
     reto3()
     reto4()
     reto5()
+    reto6()
     println("\n=== FIN ===")
 }
