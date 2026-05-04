adbed605c11dac49eda4d6c2226ad55ffe1337bb
IvanF-24 <ivanflorez134@gmail.com>
2026-05-03 23:05:05 -0500

    Fun 25

--- src/Ejercicios.kt
+++ src/Ejercicios.kt
@@ -321,5 +321,16 @@ fun reto24() {
     println("\n--- Reto 24: Contenedores Balanceados ---")
     println("Manifiesto: $manifiesto - ¿Válido?: $esValido")
 }
+// 25. Rendimiento de Maratón
+fun reto25() {
+    val tiempos = mutableListOf(125.0, 118.0, 135.0, 112.0, 128.0)
+    tiempos.sort()
+    tiempos.removeAt(0) // Mínimo
+    tiempos.removeAt(tiempos.size - 1) // Máximo
+    val promedioReal = tiempos.average()
+
+    println("\n--- Reto 25: Rendimiento Maratón ---")
+    println("Promedio sin outliers: $promedioReal")
+}
 
 
