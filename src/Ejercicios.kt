70b8143ce2eb4f7b94d9fb212988d068ba219753
IvanF-24 <ivanflorez134@gmail.com>
2026-05-03 23:13:34 -0500

    Fun 29

--- src/Ejercicios.kt
+++ src/Ejercicios.kt
@@ -372,5 +372,23 @@ fun reto28() {
     println("\n--- Reto 28: Parking ---")
     println("Costo por $horas horas: $costo")
 }
+// 29. Parejas de Viaje por Peso
+fun reto29() {
+    val maletas = listOf(12, 18, 7, 23, 9)
+    val target = 30
+    var pareja: Pair<Int, Int>? = null
+    val vistos = mutableSetOf<Int>()
 
+    for (peso in maletas) {
+        val complemento = target - peso
+        if (vistos.contains(complemento)) {
+            pareja = complemento to peso
+            break
+        }
+        vistos.add(peso)
+    }
+
+    println("\n--- Reto 29: Parejas de Carga ---")
+    println("Para target $target, pareja encontrada: $pareja")
+}
 
