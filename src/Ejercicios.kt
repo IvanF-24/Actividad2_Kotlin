84c950aa77cb9c6bc1a410d6f4e380b7914fb06e
IvanF-24 <ivanflorez134@gmail.com>
2026-05-03 22:58:50 -0500

    Fun 21

--- src/Ejercicios.kt
+++ src/Ejercicios.kt
@@ -272,4 +272,17 @@ fun reto21() {
     println("\n--- Reto 21: Pisos Primos ---")
     println("Pisos con mantenimiento: $pisosPrimos")
 }
+// 22. Pila de Platos Sucios
+fun reto22() {
+    val pila = mutableListOf<String>()
+    fun push(plato: String) = pila.add(plato)
+    fun pop(): String? = if (pila.isNotEmpty()) pila.removeAt(pila.size - 1) else null
+
+    println("\n--- Reto 22: Pila de Platos ---")
+    push("Plato Fuerte")
+    push("Plato Ensalada")
+    println("Lavando: ${pop()}")
+    println("Pila restante: $pila")
+}
+
 
